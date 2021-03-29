3f12a9c5ac41b6953b93373950287dcd56c939d8
alvaro <alvarorga@hotmail.com>
2021-03-29 16:30:12 +0200

    Add Portfolio class

--- index_tracking/get_data.py
+++ index_tracking/get_data.py
@@ -1,4 +1,4 @@
-# Get the data stored in .csv format and compute correlation matrices.
+# Get the data stored in .csv format and make Portfolio class.
 
 import numpy as np
 import pandas as pd
@@ -37,61 +37,84 @@ def read_stock_data(ticker, data_location, day_first, day_last):
     day_first = pd.to_datetime(day_first, format="%Y-%m-%d")
     day_last = pd.to_datetime(day_last, format="%Y-%m-%d")
     close = df.loc[(df.index >= day_first) & (df.index <= day_last),
-                   'Close'].to_numpy()
+                   'Close']
+    arr_close = close.to_numpy()
                 
-    r = (close[1:] - close[:-1])/close[:-1]
+    r = (arr_close[1:] - arr_close[:-1])/arr_close[:-1]
     # Convert any nan data to 0.
     r = np.nan_to_num(r, copy=False)
 
-    return r
+    return r, close.index
 
 
-def make_correlation_matrices(ix_ticker, tickers, data_location, 
-                              day_first, day_last,
-                              normalize=True):
-    """Return the correlation matrices Σ, g, ε0 from the stock returns.
-
-    The data is selected between two dates and for specific stocks in the index. 
-    If normalize is True then the matrices are normalized as:
-        Σ[i, j] -> Σ[i, j]/np.sqrt(Σ[i, i]*Σ[j, j])
-        g[i] -> g[i]/np.sqrt(Σ[i, i])
-
-    Example
-    -------
-    >>> make_correlation_matrices('OSX', ['CHX', 'CLB'], '../data/', 
-                                  '2021-01-01', '2021-02-27')
-
-    Return the normalized correlation matrices of the index 'OSX' with stocks 
-    'CSH' and 'CLB' between dates '2021-01-01' and '2021-02-27' (first day 
-    returns are not included). The data is taken from '../data/'.
+class Portfolio:
+    """Class with returns of several stocks and the index between a date range.
     
+    Attributes
+    ----------
+
+    N: int
+        Number of available stocks to purchase.
+
+    tickers: list of str
+        List with all ticker names of the stocks.
+
+    ticker_index: str
+        Ticker name of the index.
+
+    w: 1d array of floats
+        Weights of each stock in our portfolio.
+
+    n: 1d array of bools
+        Array with all purchased stocks: either true or false.
+    
+    date_range: list of str
+        Day of first returns and day of last returns.
+
+    returns: pandas DataFrame
+        Dictionary with daily returns of all available stocks indexed by date.
+
+    Σ: 2d array of floats
+        Correlation matrix of returns between date range.
+    
+    g: 1d array of floats
+        Correlation matrix of index and all stocks returns between date range.
+    
+    ε0: float
+        Correlation matrix of index returns between date range.
     """
-    # Number of stocks.
-    N = len(tickers)
 
-    # Read index data.
-    rI = read_stock_data(ix_ticker, data_location, day_first, day_last)
-    # Get number of datapoints.
-    datapts = np.size(rI, axis=0)
-    # Read stock data.
-    r = np.zeros((N, datapts))
-    for ix, ticker in enumerate(tickers):
-        r[ix] = read_stock_data(ticker, data_location, day_first, day_last)
-        
-    # Make correlation matrices
-    Σ = np.zeros((N, N))
-    g = np.zeros((N))
-    ε0 = np.dot(rI, rI)
-    for i in range(N):
-        g[i] = np.dot(r[i], rI)
-        for j in range(N):
-            Σ[i, j] = np.dot(r[i], r[j])
+    def __init__(self, tickers, ticker_index, data_location, day_first, day_last):
 
-    # Normalize.
-    if normalize is True:
-        for i in range(N):
-            g[i] /= np.sqrt(Σ[i, i])
-            for j in range(N):
-                Σ[i, j] /= np.sqrt(Σ[i, i]*Σ[j, j])
+        self.tickers = tickers
+        self.ticker_index = ticker_index 
+        self.N = len(tickers)
 
-    return Σ, g, ε0
+        # Weight of current stocks in the portfolio.
+        self.w = np.full(self.N, 1/self.N)
+        # Purchased stocks.
+        self.n = np.full(self.N, True)
+
+        # Date range of data.
+        self.date_range = [day_first, day_last]
+
+        # Read daily returns and their dates.
+        r_dict = {}
+        r_dict[ticker_index], date_index = read_stock_data(
+            ticker_index, data_location, day_first, day_last
+            )
+        for ticker in tickers:
+            r_dict[ticker], _ = read_stock_data(ticker, data_location, day_first, day_last)
+
+        # Store into DataFrame, remove first day from return dates.
+        self.returns = pd.DataFrame(data=r_dict, index=date_index[1:])
+
+        # Make correlation matrices
+        self.Σ = np.zeros((self.N, self.N))
+        self.g = np.zeros((self.N))
+        self.ε0 = np.dot(self.returns[ticker_index], self.returns[ticker_index])
+
+        for i, ticker_i in enumerate(tickers):
+            self.g[i] = np.dot(self.returns[ticker_i], self.returns[ticker_index])
+            for j, ticker_j in enumerate(tickers):
+                self.Σ[i, j] = np.dot(self.returns[ticker_i], self.returns[ticker_j])
